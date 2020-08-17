9f7de4a9061da31e139f33ee2b12e45c5366c01f
lishuo <thinklis@163.com>
2020-08-17 21:27:07 +0800

    'git初始化'

---
+++ 01test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+ Author: lishuo
+ Date: 2020-08-17 14:08:22
+ LastEditors: lishuo
+ LastEditTime: 2020-08-17 14:09:54
+ Description: '第一天练习python编程'
+ FilePath: \python_code\01test.py
+'''
+
+print('4. ', bool('1'))
+print('5. ', str(32) == '32')
+print('6. ', int(6.26))
+print('7. ', float(32))
+print('8. ', float("3.21"))
+print('9. ', int('434'))
+print('10. ', int('3'))
+print('11. ', bool(-1))   # 为什么这个时true呢
+print('12. ', bool(""))
+print('13. ', bool(0))
+print('14. ', 'wrqq' > 'acd')
+print('15. ', 'ttt' == 'ttt ')
+print('16. ', 'wer'*3)
+print('17. ', 'wer' + '2222')
