dee6ce535a458fe44e2b2ed7082c92df79f52727
lishuo <thinklis@163.com>
2020-08-19 07:46:35 +0800

    'dev分支上修改1'

--- 01test.py
+++ 01test.py
@@ -4,7 +4,7 @@
  Author       : lishuo
  Date         : 2020-08-17 14:08:22
  LastEditors  : lishuo
- LastEditTime : 2020-08-18 22:01:13
+ LastEditTime : 2020-08-19 07:46:04
  Description  : 第一天练习python编程
  FilePath     : \\python_code\\01test.py
 '''
@@ -27,3 +27,5 @@ print('17. ', 'wer' + '2222')
 
 print('修改01test.py文件，目的练习git的使用')
 print('第二次修改test.py文件')
+
+print('这里是我在dev分支上的修改1')
